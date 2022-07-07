fff72c48ea7a93a56f6a0dd2716e2177e4728dda
9Alex_Hu <9Alex_Hu@singaporeair.com.sg>
2022-07-07 10:47:15 +0800

    feat: update main page layouts, and gitignore file

--- mvvm-swiftui/Weather/WeatherApp/Views/ContentView.swift
+++ mvvm-swiftui/Weather/WeatherApp/Views/ContentView.swift
@@ -8,24 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var weatherViewModel: WeatherViewModelType = WeatherViewModel()
+    @StateObject var weatherViewModel = WeatherViewModel()
     @State var searchCity = "Taipei"
 //    var weatherCoordinator: WeatherCoordinator!
     
     var body: some View {
-        TextField("", text: $searchCity)
-            .padding(.leading, 50)
-            .font(.system(size: 20.0))
-        Button {
-            weatherViewModel.updateCity(searchCity)
-        } label: {
-            Text("Search City")
-        }
-        
-        Text("\(weatherViewModel.weather.main.temperature)°C")
-            .padding()
-        Text(weatherViewModel.city)
-            .padding()
+        VStack(alignment: .leading) {
+            HStack(alignment: .center, spacing: 20) {
+                TextField("", text: $searchCity)
+                    .frame(width: UIScreen.main.bounds.size.width * 3 / 5, height: 25, alignment: .leading)
+                    .font(.system(size: 20.0))
+                    .border(.secondary)
+                Button {
+                    weatherViewModel.city = searchCity
+                } label: {
+                    Text("Search")
+                }
+            }
+            VStack(alignment: .leading) {
+                Section(header: Text("Result").font(.title)) {
+                    Text(weatherViewModel.city)
+                        .alignmentGuide(.leading, computeValue: { d in
+                            d[.leading]
+                        })
+                        .padding([.top], 15)
+                        .padding([.bottom], 10)
+                    Text("\(weatherViewModel.weather.main.temperature)°C")
+                        .alignmentGuide(.leading, computeValue: { d in
+                            d[.leading]
+                        })
+                }
+            }
+            Spacer()
+        }.padding([.top], 30)
     }
 }
 
