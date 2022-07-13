db3795cf793be3d8d3f90489257240fac06ed908
9Alex_Hu <9Alex_Hu@singaporeair.com.sg>
2022-07-13 10:11:23 +0800

    feat: Update content view layout with ScrollView and stack. [close #2]

--- mvvm-swiftui/Weather/WeatherApp/Views/ContentView.swift
+++ mvvm-swiftui/Weather/WeatherApp/Views/ContentView.swift
@@ -13,19 +13,22 @@ struct ContentView: View {
 //    var weatherCoordinator: WeatherCoordinator!
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack(alignment: .center, spacing: 20) {
-                TextField("", text: $searchCity)
-                    .frame(width: UIScreen.main.bounds.size.width * 3 / 5, height: 25, alignment: .leading)
-                    .font(.system(size: 20.0))
-                    .border(.secondary)
-                Button {
-                    weatherViewModel.city = searchCity
-                } label: {
-                    Text("Search")
+        ScrollView {
+            LazyVStack(alignment: .center) {
+                HStack(alignment: .center, spacing: 20) {
+                    TextField("", text: $searchCity)
+                        .frame(width: UIScreen.main.bounds.size.width * 3 / 5, height: 25, alignment: .leading)
+                        .font(.system(size: 20.0))
+                        .border(.secondary)
+                    Button {
+                        weatherViewModel.city = searchCity
+                    } label: {
+                        Text("Search")
+                    }
                 }
             }
-            VStack(alignment: .leading) {
+            
+            LazyVStack(alignment: .leading) {
                 Section(header: Text("Result").font(.title)) {
                     Text(weatherViewModel.city)
                         .alignmentGuide(.leading, computeValue: { d in
@@ -38,9 +41,37 @@ struct ContentView: View {
                             d[.leading]
                         })
                 }
-            }
-            Spacer()
-        }.padding([.top], 30)
+            }.padding([.leading], 20)
+        }
+        
+//        VStack(alignment: .leading) {
+//            HStack(alignment: .center, spacing: 20) {
+//                TextField("", text: $searchCity)
+//                    .frame(width: UIScreen.main.bounds.size.width * 3 / 5, height: 25, alignment: .leading)
+//                    .font(.system(size: 20.0))
+//                    .border(.secondary)
+//                Button {
+//                    weatherViewModel.city = searchCity
+//                } label: {
+//                    Text("Search")
+//                }
+//            }
+//            VStack(alignment: .leading) {
+//                Section(header: Text("Result").font(.title)) {
+//                    Text(weatherViewModel.city)
+//                        .alignmentGuide(.leading, computeValue: { d in
+//                            d[.leading]
+//                        })
+//                        .padding([.top], 15)
+//                        .padding([.bottom], 10)
+//                    Text("\(weatherViewModel.weather.main.temperature)°C")
+//                        .alignmentGuide(.leading, computeValue: { d in
+//                            d[.leading]
+//                        })
+//                }
+//            }
+//            Spacer()
+//        }.padding([.top], 30)
     }
 }
 
